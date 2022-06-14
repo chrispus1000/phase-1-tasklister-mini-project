b7a078d4ade4a36ac8809eca4d40aba4c65c7694
chrispus1000 <minrevac@gmail.com>
2022-06-15 02:54:56 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded",() =>{
+  const input = document.getElementById('new-task-description');
+  const submit = document.getElementById(`button`);
+  const  theList = document.getElementById(`tasks`);
+  
+  const submitButton = (e) => {
+    const text = input.value;
+    const theNew = document.createElement(`li`);
+    theNew.innerHTML = text;
+    e.preventDefault();
+   
+    //console.log("I was clicked ");
+  
+    theList.appendChild(theNew);
+  };
+    submit.addEventListener(`click`, submitButton);
+   // submit.preventDefault()
+   
+      if (submit !== null ){
+        console.log( "I was clicked ")
+        }
+     })
